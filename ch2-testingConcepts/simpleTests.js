022d3793e5610346097334b23270e412c65536f5
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-01 17:45:38 -0400

    added another case

--- ch2-testingConcepts/simpleTests.js
+++ ch2-testingConcepts/simpleTests.js
@@ -3,8 +3,8 @@ YUI().use('test-console', 'test', function (Y) {
     var suite = new Y.Test.Suite("Simple currency conversion suite");
 
     //tests go here
-    var testCase = new Y.Test.Case({
-        name: "Simple currency conversion case",
+    var testINRCase = new Y.Test.Case({
+        name: "Simple INR currency conversion case",
         
         setUp: function () {
             this.expectedResult = 1.59;
@@ -18,13 +18,30 @@ YUI().use('test-console', 'test', function (Y) {
             Y.Assert.areEqual(this.expectedResult, convertCurrency(100, 1 / 63), "100 INR should be equal to $ 1.59");
         }
     });
+    
+    var testFakeCase = new Y.Test.Case({
+        name: "Simple Fake currency conversion case",
+        
+        setUp: function(){
+            this.expectedResult = 1.00;
+        },
+        
+        tearDown: function (){
+            delete this.expectedResult;
+        },
+        
+        testData: function (){
+            Y.Assert.areEqual(this.expectedResult, convertCurrency(100, 1/100), "100 Fake should be equal to $ 1.00");
+        }
+    })
 
     //render the test console
     new Y.Test.Console({
         newestOnTop: false,
     }).render('#testLogs');
 
-    suite.add(testCase);
+    suite.add(testINRCase);
+    suite.add(testFakeCase);
 
     Y.Test.Runner.add(suite);
 
