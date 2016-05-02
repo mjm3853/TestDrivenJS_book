b261ef49a5ebd545455bb5fcf8f8253a7b504fc5
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-01 22:47:40 -0400

    success with karma and jasmine

--- ch3-testingTools/Karma with Jasmine/karma.conf.js
+++ ch3-testingTools/Karma with Jasmine/karma.conf.js
@@ -10,12 +10,14 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'test-main.js'
+      'test-main.js',
+      'js/currency-conversion.js',
+      'test/*.js'
     ],
 
 
@@ -60,7 +62,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
