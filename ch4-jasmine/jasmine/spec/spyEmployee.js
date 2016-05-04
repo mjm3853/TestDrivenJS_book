45d7ba667231e2c6a264b2d4a44353fa5d7ceb77
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-03 22:29:36 -0400

    spies understood and added call through

--- ch4-jasmine/jasmine/spec/spyEmployee.js
+++ ch4-jasmine/jasmine/spec/spyEmployee.js
@@ -5,5 +5,14 @@ describe("Jasmine Spy", function () {
         spyOn(alice, "calculateSalary");
         console.log(alice.getDetails());
         expect(alice.calculateSalary).toHaveBeenCalled();
-    })
+    });
+    
+    it("Spying Employee with call through", function(){
+       var alice = new Employee("Alice", 4, "Testing");
+       spyOn(alice, "calculateSalary").and.callThrough();
+       var salary = alice.getSalary();
+       expect (alice.calculateSalary).toHaveBeenCalled();
+       console.log("Salary is : " + salary);
+       expect(salary).toEqual(4000);
+    });
 });
