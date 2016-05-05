0cfdcb24be031a01053f90d680add831a2396323
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-04 22:58:09 -0400

    added test with a spy having a return value

--- ch4-jasmine/jasmine/spec/spyEmployee.js
+++ ch4-jasmine/jasmine/spec/spyEmployee.js
@@ -6,13 +6,21 @@ describe("Jasmine Spy", function () {
         console.log(alice.getDetails());
         expect(alice.calculateSalary).toHaveBeenCalled();
     });
-    
-    it("Spying Employee with call through", function(){
-       var alice = new Employee("Alice", 4, "Testing");
-       spyOn(alice, "calculateSalary").and.callThrough();
-       var salary = alice.getSalary();
-       expect (alice.calculateSalary).toHaveBeenCalled();
-       console.log("Salary is : " + salary);
-       expect(salary).toEqual(4000);
+
+    it("Spying Employee with call through", function () {
+        var alice = new Employee("Alice", 4, "Testing");
+        spyOn(alice, "calculateSalary").and.callThrough();
+        var salary = alice.getSalary();
+        expect(alice.calculateSalary).toHaveBeenCalled();
+        console.log("Salary is : " + salary);
+        expect(salary).toEqual(4000);
     });
+
+    it("Spying Employee with return value", function () {
+        var alice = new Employee("Alice", 4, "Testing");
+        spyOn(alice, "calculateSalary").and.returnValue(9999);
+        alice.calculateSalary();
+        expect(alice.calculateSalary).toHaveBeenCalled();
+        expect(alice.calculateSalary()).toEqual(9999);
+    })
 });
