59d06c80259cffd1ec8ad75ea30dcdb02a47589b
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-07 19:36:36 -0400

    using the spy calls property

--- ch4-jasmine/jasmine/spec/spyEmployee.js
+++ ch4-jasmine/jasmine/spec/spyEmployee.js
@@ -26,12 +26,34 @@ describe("Jasmine Spy", function () {
 
     it("Spying Employee with fake call", function () {
         var alice = new Employee("Alice", 4, "Testing");
-        spyOn(alice, "calculateSalary").and.callFake(function(grade){
-           var tSalary = 1000;
-           return tSalary * grade; 
+        spyOn(alice, "calculateSalary").and.callFake(function (grade) {
+            var tSalary = 1000;
+            return tSalary * grade;
         });
         var salary = alice.calculateSalary(10);
         expect(alice.calculateSalary).toHaveBeenCalled();
         expect(salary).toEqual(10000);
+    });
+
+    it("Tracking spies with calls property", function () {
+        var alice = new Employee("Alice", 4, "Testing");
+        
+        spyOn(alice, "calculateSalary").and.callThrough();
+        var salary = alice.getSalary();
+        
+        alice.calculateSalary.and.stub();
+        salary = alice.getSalary();
+        expect(salary).toEqual(4000);
+        
+        expect(alice.calculateSalary.calls.any()).toEqual(true);
+        expect(alice.calculateSalary.calls.count()).toEqual(1);
+        console.log(alice.calculateSalary.calls.argsFor(0));
+        expect(alice.calculateSalary.calls.argsFor(0)).toEqual([]);
+        
+        alice.calculateSalary(1000);
+        
+        console.log(alice.calculateSalary.calls.argsFor(1));
+        expect(alice.calculateSalary.calls.allArgs());
+        expect(alice.calculateSalary.calls.allArgs()).toEqual([[], [1000]]);
     });
 });
