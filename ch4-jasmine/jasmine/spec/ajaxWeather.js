6c2414d85879b2a7b03ad13168d4621b755aea8b
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-07 21:55:58 -0400

    mocking ajax and testing it

---
+++ ch4-jasmine/jasmine/spec/ajaxWeather.js
@@ -0,0 +1,42 @@
+
+describe('Using mock-ajax', () => {
+
+    beforeEach(() => {
+        jasmine.Ajax.install();
+    });
+
+
+    afterEach(() => {
+        jasmine.Ajax.uninstall();
+    });
+
+
+    it('Checking weather report with AJAX API', () => {
+        var successFuction = jasmine.createSpy("success");
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function (args) {
+            if (this.readyState == this.DONE) {
+                successFuction(this.responseText);
+            }
+        };
+        xhr.open("GET", "/get/weather/IN-Mumbai");
+        xhr.send();
+
+        expect(jasmine.Ajax.requests.mostRecent().url).toBe('/get/weather/IN-Mumbai');
+
+        expect(successFuction).not.toHaveBeenCalled();
+
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            "status": 200,
+            "contentType": 'text/plain',
+            "responseText": 'Temp 25 C, Sunlight'
+        });
+
+        expect(successFuction).toHaveBeenCalledWith('Temp 25 C, Sunlight');
+
+    });
+
+
+
+
+});
