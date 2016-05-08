6e49307310425b1beed3ae33919d3d16cc5a2f52
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-05-07 22:04:33 -0400

    simple example of nested suites with no tests

---
+++ ch4-jasmine/jasmine/spec/nestedSuites.js
@@ -0,0 +1,35 @@
+
+describe('Testing Company Wide functions', () => {
+    
+    describe('Testing HR Department functions', () => {
+        
+        describe('Testing HR Department functions - Prize Distributions', () => {
+            
+        });
+            
+    });
+    
+    describe('Testing Admin Department functions', () => {
+        
+        describe('Testing Admin Department functions - Scheduling', () => {
+            
+        });
+        
+        describe('Testing Admin Department functions - Organizing', () => {
+            
+        });
+            
+    });
+    
+    describe('Testing Finance Department functions', () => {
+        
+        describe('Testing Finance Department function - Making Money', () => {
+            
+        });
+            
+        
+    });
+        
+        
+});
+    
